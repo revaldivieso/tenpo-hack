e04eb80eeab91cd79869be31ded6990a5d7295a3
MonicaBartha <barthamonica@gmail.com>
2020-05-14 18:09:43 -0400

    cree componente nav, primera vista y carpeta media

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import AccesibleNav from './Components/AccesibleNav';
+import Nr1 from './Vistas/Nr1';
 
 function App() {
   return (
     <div className="App">
-     
+     <AccesibleNav />
+     <Nr1 />
     </div>
   );
 }
---
+++ src/Vistas/Nr1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../Media/logo.jpg";
+
+class Nr1 extends React.Component {
+  render() {
+    return (
+      <div className="view-nr-1">
+        <img src={Logo} alt="logo tenpo" className="logo-view-nr1"></img>
+        <h1>¡Hola!</h1>
+        <h2>Te damos la bienvenida</h2>
+      </div>
+    );
+  }
+}
+export default Nr1;
